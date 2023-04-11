68103c974a6ba60ffe7a35f5bb19e8568b10f073
KazKB <kaleelboston@gmail.com>
2023-04-11 16:37:51 +0100

    Updated Tablet Navigation

--- src/javascript/navigation.js
+++ src/javascript/navigation.js
@@ -2,7 +2,7 @@ let menuButton = document.querySelector("header p");
 let links = document.querySelectorAll("nav a");
 let nav = document.querySelector("nav");
 
-if(screen.width < 1000) {
+if(screen.width < 700) {
     menuButton.addEventListener("click", function() {
         menuButton.classList.toggle("clicked");
         for(let e = 0; e < links.length; e++) {
